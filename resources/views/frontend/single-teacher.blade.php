79fbe8a8dce6b4368cb9403f9431807458a339d9
IDLKZ <idlkz2019@gmail.com>
2021-04-16 23:19:55 +0600

    nobody is perfect but Tima

--- resources/views/frontend/single-teacher.blade.php
+++ resources/views/frontend/single-teacher.blade.php
@@ -1,5 +1,13 @@
 @extends("frontend.layout.layout")
-
+@push('styles')
+    <style>
+        @media only screen and (min-width: 768px) {
+            .teacher-img {
+                position: absolute;width: 70%; height: 500px; z-index: 1000; top: -200px;
+            }
+        }
+    </style>
+@endpush
 @section('content')
     <section class="page-title page-title-layout6 text-center bg-overlay bg-overlay-gradient bg-parallax">
         <div class="bg-img"><img src="/assets/images/page-titles/3.jpg" alt="background"></div>
@@ -9,7 +17,7 @@
         <div class="container">
             <div class="row py-4 my-4">
                 <div class="col-md-6 d-flex justify-content-center align-items-center">
-                    <img src="{{$teacher->img}}" style="position: absolute;width: 70%; height: 500px; z-index: 1000; top: -200px;">
+                    <img class="teacher-img" src="{{$teacher->img}}">
                 </div>
                 <div class="col-md-6">
                     <h1 class="heading-1">
