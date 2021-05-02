d431e5fd78113996f653b05522032274bfc7e5f3
IDLKZ <idlkz2019@gmail.com>
2021-05-02 17:37:53 +0600

    nobody is perfect but Tima

--- resources/views/frontend/teacher.blade.php
+++ resources/views/frontend/teacher.blade.php
@@ -5,9 +5,16 @@
         <div class="bg-img"><img src="/assets/images/page-titles/3.jpg" alt="background"></div>
         <div class="container">
             <div class="row">
-                <div class="col-12">
-                    <h1 class="pagetitle__heading mb-0">{{$header->title}}</h1>
-                </div><!-- /.col-12 -->
+                @if ($header)
+                    <div class="col-12">
+                        <h1 class="pagetitle__heading mb-0">{{$header->title}}</h1>
+                    </div><!-- /.col-12 -->
+                @else
+                    <div class="col-12">
+                        <h1 class="pagetitle__heading mb-0">{{__('frontend.teacher_title')}}</h1>
+                    </div><!-- /.col-12 -->
+                @endif
+
             </div><!-- /.row -->
         </div><!-- /.container -->
     </section><!-- /.page-title -->
