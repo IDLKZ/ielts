8fff45cebcf19e37fd23b5abde847b7236ecdeec
IDLKZ <idlkz2019@gmail.com>
2021-05-03 22:01:24 +0600

    nobody is perfect but Tima

--- resources/views/frontend/ielts.blade.php
+++ resources/views/frontend/ielts.blade.php
@@ -6,7 +6,11 @@
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <h1 class="pagetitle__heading mb-0">{{$header->title}}</h1>
+                    @if ($header)
+                        <h1 class="pagetitle__heading mb-0">{{$header->title}}</h1>
+                    @else
+                        <h1 class="pagetitle__heading mb-0">{{__('frontend.ielts_title')}}</h1>
+                    @endif
                 </div><!-- /.col-12 -->
             </div><!-- /.row -->
         </div><!-- /.container -->
